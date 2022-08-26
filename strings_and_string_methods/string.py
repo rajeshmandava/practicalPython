752fdeebc167e5595ce19efcaf4baab187a05364
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-26 12:31:13 +0200

    Concatenation, Indexing and Slicing

--- strings_and_string_methods/string.py
+++ strings_and_string_methods/string.py
@@ -45,3 +45,67 @@ string4 = "This is line number one \
   continuing in line number two"
 
 print(string4)
+
+# 4.2 Concatenation, Indexing and Slicing
+
+# 1. Concatenation, which joins two strings together
+# 2. Indexing, which gets a single character from a string
+# 3. Slicing, which gets several characters from a string at once
+
+string1 = "Hello"
+string2 = "world!"
+magic_string = string1 + string2
+print(magic_string)
+
+first_name = "Arthur"
+last_name = "Dent"
+full_name = first_name + " "+last_name
+print(full_name)
+
+# String indexing
+flavor = "fig pie"
+print(flavor[1])
+print(flavor[0])
+
+final_index = len(flavor)-1
+last_character  = flavor[final_index]
+
+print(final_index)
+print(last_character)
+
+# String slicing
+first_three_letters = flavor[0] + flavor[1] +flavor[2]
+print(first_three_letters)
+print(flavor[0:3])
+print(flavor[:3])
+print(flavor[3:])
+print(flavor[:])
+print(flavor[13:15])
+
+# Strings are Immutable
+word = "goal"
+# word[0] ='f' TypeError : 'str' object does not support item assignment
+
+word = "f"  + word[1:]
+print(word)
+
+# Review Exercises
+
+# 1. Create a string and print its length using len()
+string1 = "This is string"
+print(len(string1))
+
+# 2. Create two strings, concatenate them, and print the resulting string.
+# 3. Create two strings, use concatenation to add a space between them, and print the result
+string1 = "This is string"
+string2 = "Concatenation"
+
+multi_string = string1+" "+ string2
+
+print(multi_string)
+
+# 4. Print the string "zing" by using slice notation to specify the correct range of characters in the string "zbazinga"
+string1 = "bazinga"
+last_index = len(string1)-1
+print(last_index)
+print(string1[2:last_index])
