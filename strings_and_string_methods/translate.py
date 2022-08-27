093c81aeae5a4aaf6d32e0973a1ab37f8fe4d4b5
Rajesh Mandava <rajesh.mandava07@gmail.com>
2022-08-27 10:32:34 +0200

    Challange: Turn your user into L33t and H4x0r

---
+++ strings_and_string_methods/translate.py
@@ -0,0 +1,11 @@
+# Challange: Turn your user into L33t H4x0r
+string1 = input("Enter some text:")
+string1 = string1.replace("a","4")
+string1 = string1.replace("b","8")
+string1 = string1.replace("e","3")
+string1 = string1.replace("l","1")
+string1 = string1.replace("o","0")
+string1 = string1.replace("s","5")
+string1 = string1.replace("t","7")
+
+print(string1)
